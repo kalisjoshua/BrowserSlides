e5e6c94a803fe319ed35deef201b07d68c06a8da
Joshua T Kalis <kalisjoshua@gmail.com>
2012-08-27 22:53:43 -0400

    adding highlighting

--- js/ssmd.js
+++ js/ssmd.js
@@ -1,9 +1,19 @@
 
-(function () {
+(function (global) {
     "use strict";
 
     var nl = "\n"
       , types = {"#":"ol","*":"ul"};
+
+    function highlight(js) {
+        return js
+            .replace(/(\/\/[^$]*)/gm, '<span class="comment">$1</span>')
+            .replace(/('.*?')/gm, '<span class="string">$1</span>')
+            .replace(/(\b[\-+]?\d+(\.\d+)?)/gm, '<span class="number">$1</span>')
+            .replace(/\bnew\s+(\w+)/gm, '<span class="keyword">new</span> <span class="init">$1</span>')
+            .replace(/\breturn\b/, '<span class="init">return</span>')
+            .replace(/\b(function|new|throw|var|if|else)\b/gm, '<span class="keyword">$1</span>');
+    }
 
     function listType (leader) {
         return types[leader.slice(-1)];
@@ -16,33 +26,24 @@
             .replace(/%2/, content);
     }
 
-    window.ssmd = function (txt) {
-        
-        return txt
+    function md (str) {
+        return str
             // remove leading whitespace
-            .replace(/^\s*/, "")
+            // .replace(/^\s*/, "")
 
             // remove blank lines
-            .replace(/\n+/gm, "\n")
+            // .replace(/\n+/gm, "\n")
 
             // remove trailing closing _
             .replace(/\n_\n*$/, "")
 
-            // title (h1-6) tags
-            .replace(/^(=+)\s+(.*)/gm, function (match, p1, p2) {
-                return tagify("h" + p1.length, p2);
-            })
-
             // code blocks
             .replace(/(^-{3})\n+([^\1]*?)\n+\1/gm, function (match, garbage, code) {
-                return tagify("pre", tagify("code", code.split(nl).join("<br/>")));
+                return tagify("pre", tagify("code", code.split(nl).map(highlight).join("~br~")));
             })
 
             // blockquote
-            .replace(/^\s{4}((['"]).*\1)\s*~~\s*(.*)$/gm, tagify("blockquote", tagify("p", "$1") + tagify("footer", "$3")))
-
-            // paragraphs
-            .replace(/^([^#\*<].+)$/gm, tagify("p", "$1"))
+            .replace(/^\s{4}((['"]).*\1)\s+~~\s*(.*)$/gm, tagify("blockquote", tagify("p", "$1") + tagify("footer", "$3")))
 
             // lists
             .replace(/^(?:([#\*]+)\s*[^$\n]+[\n\W\D])+/gm, function (list, leader) {
@@ -59,7 +60,7 @@
                             temp += "<%1>".replace("%1", listType(leader));
                         } else if (leader.length < previous.length) {
                             // close the previous list item and list
-                            temp += "</li></%1>".replace("%1", stack.pop());
+                            temp += "</li></%1></li>".replace("%1", stack.pop());
                         } else if (leader.length === previous.length) {
                             // just close the previous list item
                             temp += "</li>";
@@ -86,8 +87,26 @@
                 return list + nl;
             })
 
+            // title (h1-6) tags
+            .replace(/^(=+)\s+(.*)/gm, function (match, p1, p2) {
+                return tagify("h" + p1.length, p2);
+            })
+
+            // paragraphs
+            .replace(/^([^#\*<].+)$/gm, function (match, content) {
+                content = content.replace(/(?:^\s*)|(?:\s*$)/, "");
+                return (/^</).test(content) ? content : tagify("p", content);
+            })
+
+            .replace(/~br~/gm, "\n")
+
             // links
-            .replace(/\[([^\]]+)\]\(([^\(]+)\)/g, tagify("a", "$1", " href=\"" + "$2" + "\""))
+            .replace(/\[([^\]]+)\]\(([^\)]+)\)/g, tagify("a", "$1", " href=\"" + "$2" + "\""));
+    }
+
+    global.ssmd = function (txt) {
+        
+        return md(txt)
 
             // split on slide terminator
             .split("\n_\n")
@@ -99,4 +118,14 @@
             // join into one string
             .join(nl);
     };
-}());
+
+    global.ssmd.md = md;
+
+}((function () {
+    "use strict";
+    try {
+        return exports;
+    } catch (e) {
+        return window;
+    }
+}())));
